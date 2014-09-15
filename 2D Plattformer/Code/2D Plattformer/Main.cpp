48d303e7c50a52ffa53c31b1c1a0af5aa0e76f3f
Pontus Fransson <pontus.fransson92@gmail.com>
2014-09-15 22:14:35 +0200

    Added Render Engine project, First window for game

---
+++ 2D Plattformer/Code/2D Plattformer/Main.cpp
@@ -0,0 +1,79 @@
+#include <Windows.h>
+
+const unsigned int SCREEN_WIDTH = 1024;
+const unsigned int SCREEN_HEIGHT = 768;
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	if (SetDllDirectory(L"..\\DLL") == FALSE)
+	{
+		return -1;
+	}
+
+	HWND hWnd;
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.lpszClassName = L"WindowClass";
+
+	RegisterClassEx(&wc);
+
+	RECT wr = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+	hWnd = CreateWindowEx(NULL,
+		L"WindowClass",
+		L"2D Plattformer",
+		WS_OVERLAPPEDWINDOW,
+		300,
+		300,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+
+	MSG msg;
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				break;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		//Update(1.0f);
+		//Render();
+	}
+
+	return 0;
+}
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+	{
+					   PostQuitMessage(0);
+					   return 0;
+	} break;
+
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
